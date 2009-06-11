95fe64c9b657db1c9d7a031565941eccbe17743b
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2009-06-11 12:05:37 +0000

    flu vignette and functions logbook and all.equal
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/hyperSpec.R
+++ pkg/R/hyperSpec.R
@@ -538,6 +538,16 @@ setMethod ("dimnames", "hyperSpec", function (x){
 		})
 ###-----------------------------------------------------------------------------
 ###
+###  logbook
+###  
+###
+logbook <- function (x){
+	.is.hy (x)
+	validObject (x)
+	x@log
+}
+###-----------------------------------------------------------------------------
+###
 ###  labels
 ###  
 ###  
@@ -902,6 +912,47 @@ setMethod ("Compare", signature (e1 = "hyperSpec", e2 = "matrix"), .compx)
 
 setMethod ("Compare", signature (e1 = "numeric", e2 = "hyperSpec"), .compy)
 setMethod ("Compare", signature (e1 = "matrix", e2 = "hyperSpec"), .compy)
+###-----------------------------------------------------------------------------
+###
+###  all.equal
+###  
+###  
+
+setMethod ("all.equal", signature (target = "hyperSpec", current = "hyperSpec"),
+ function (target, current, ..., check.column.order = FALSE, check.label = FALSE, check.log = FALSE){
+	validObject (target)
+	validObject (current)
+	
+	result <- character (0)
+	
+	cmp <- all.equal (target@wavelength, current@wavelength, ...)
+	if (! isTRUE (cmp)) result <- c("@wavelength:", cmp)
+		
+	if (check.column.order)
+		cmp <- all.equal (target@data, current@data, ...)
+	else
+		cmp <- all.equal ( target@data[order (colnames ( target@data))], 
+				          current@data[order (colnames (current@data))], ...)
+	if (! isTRUE (cmp)) result <- c (result, "@data:", cmp)
+	
+	if (check.label){
+		cmp <- all.equal (target@label[order (names (target@label))], 
+				current@label[order (names (current@label))], ...)
+		if (! isTRUE (cmp)) result <- c (result, "@label:", cmp)		
+	}
+	
+	if (check.log) {
+		cmp <- all.equal (target@log, current@log, ...)
+		if (! isTRUE (cmp)) result <- c (result, "@log:", cmp)
+	}
+	
+	if (length (result) == 0)
+		TRUE
+	else
+		result 
+}
+)
+
 
 ###-----------------------------------------------------------------------------
 ###
