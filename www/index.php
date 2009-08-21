fb0408b6a2d88f215bb7e9419ddbd727189ebfd3
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2009-08-21 16:04:36 +0000

    web page
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@93 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -26,15 +26,13 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <!-- R-Forge Logo -->
 <table border="0" width="100%" cellspacing="0" cellpadding="0">
 <tr>
-	<td style="width:212px"><img src="hyperSpec-logo.png" width="212px" height="210px", alt = "hyperSpec logo"></td>
-	<td align ="center"><?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
-</td>
+	<td><img src="hyperSpec-logo.png" alt = "hyperSpec logo"></td>
+	<td align ="center">
+		 <h1>Welcome <tt>hyperSpec</tt>!</h1>
+		 <p><tt>hyperSpec</tt> is a R package to handle hyperspectral data, i.e. spectra plus further information such as spatial information, time, concentrations, etc.</p>
+		 <p>Such data are frequently encountered in Raman, IR, NIR, UV/VIS, NMR, mass spectroscopy, AAS / AES, ...</p>
+		 <p><tt>hyperSpec</tt> acts as an interface for the convenient handling of the spectra. Chemometric data analysis is done by R and other R packages.</p>
+	</td>
 	<td align ="right"><a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td>
 </tr>
 </table>
@@ -42,13 +40,13 @@ echo $contents; } ?>
 
 <p> The package is now publicly accessible from the <a href="http://r-forge.r-project.org/plugins/scmsvn/viewcvs.php/?root=hyperspec">SVN repository</a>. </p>
 
-<h2>Installation</h2>
-<h3>... from inside R</h3>
+<h1>Installation</h1>
+<h2>... from inside R</h2>
 <p>To install the latest nightly build, type in R: 
 <pre>install.packages("hyperSpec",repos="http://R-Forge.R-project.org")</pre>
 </p>
 
-<h3>... from svn source</h3>
+<h2>... from svn source</h2>
 <ol><li>
 get an svn checkout using your favourite svn client program, or<br/>
 <tt>svn checkout svn://svn.r-forge.r-project.org/svnroot/hyperspec</tt></li>
@@ -57,7 +55,7 @@ get an svn checkout using your favourite svn client program, or<br/>
     where <tt><i>pkg-directory</i></tt> is the subdirectory called pkg of the directory where the svn checkout went.</li>
 </ol>
 
-<h3>... via source Package</h3>
+<h2>... via source Package</h2>
 <ol>
 <li><a href="hyperSpec_0.7.tar.gz">Download</a> the prebuilt .tar.gz source package.<br/>
 	 Windows users will probably need the <a href="hyperSpec_0.7.zip">compiled .zip package</a></li>
@@ -68,7 +66,7 @@ get an svn checkout using your favourite svn client program, or<br/>
 These may be a bit outdated, but they work if the nightly build fails.
 
 
-<h2>Vignettes</h2>
+<h1>Vignettes</h1>
 <p><tt>hyperSpec</tt> comes with some vignettes:</p>
 <ul>
 <li><a href="https://r-forge.r-project.org/plugins/scmsvn/viewcvs.php/*checkout*/Vignettes/intro/introduction.pdf?root=hyperspec">Introduction</a>: a user manual from aspectroscopist's point of view</li>
@@ -78,7 +76,14 @@ These may be a bit outdated, but they work if the nightly build fails.
 <li>A more technical <a href="https://r-forge.r-project.org/plugins/scmsvn/viewcvs.php/*checkout*/Vignettes/baseline/baselinebelow.pdf?root=hyperspec">explanation of the baseline fitting technique used by <tt>spc.fit.poly.below</tt></a></li>
 </ul> 
 <p> In the standard <tt>hyperSpec</tt> installation, .pdf files and source  are already available in the documentation directory, with the exception of the the chondrocyte raw data (see above).</p>
-</p>
+
+
+<h1>Useful Packages for Chemometric Data Analysis</h1>
+<p>By no means complete, if you have suggestions, send me an email.</p>
+<ul>
+<li><tt>R.matlab</t>: Read &ampersand; write .mat files, run external calculations in Matlab</li>
+<li><tt>pls</t>: Partial Least Squares regression</li>
+</ul>
 
 <p>
 Claudia Beleites<br/>
