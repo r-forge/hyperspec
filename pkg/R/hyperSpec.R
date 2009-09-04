3d3530d6735be6ba54e4608b256e60fa810d8dc4
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2009-09-04 09:53:21 +0000

    loading message
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@108 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/hyperSpec.R
+++ pkg/R/hyperSpec.R
@@ -11,8 +11,8 @@
 	vers <- paste("V. ", desc$Version)
 	cat ("Package ",  desc$Package, ", version ", desc$Version, "\n\n",
 			"To get started, try\n",
-			'   help ("hyperSpec") \n',
-			'   help (package = "hyperSpec")\n',
+			'   vignette ("introduction", package = "hyperSpec")\n',
+			'   package?hyperSpec \n',
 			'   vignette (package = "hyperSpec")\n\n',
 			"If you use this package please cite it appropriately.\n",
 			"   citation(\"hyperSpec\")\nwill give you the correct reference.", "\n\n",
