4aa18dc65e5c52148bf1e6e956ff951248e4825d
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2009-12-17 17:42:48 +0000

    levelplot and related functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@158 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 49e2e99238f905d48fd62521d6bb13babe1d9496

---
+++ pkg/R/levelplot.R
@@ -0,0 +1,156 @@
+#################################################################################
+###
+###  levelplot.R - everything that has to do with levelplot-like plotting:
+###                * levelplot
+###                * plotmap
+###                * plotvoronoi
+###                * map.identify
+###  Time-stamp: <Claudia Beleites on Thursday, 2009-12-17 at 18:33:47 on cb>
+###  
+###  levelplot is used by plotmap, plotmat, plotvoronoi
+###  
+###  Version 1.0  2009-12-16 15:04  Claudia Beleites  Claudia.Beleites@gmx.de
+###
+#################################################################################
+
+### the workhorse function
+.levelplot <- function (x, data, transform.factor = TRUE, ...) {
+  validObject (data)
+
+  ## parse formula to find the columns to be plotted
+  ## they may include also "wavelength" => see plotmat
+  parsed.formula <- latticeParseFormula (x, as.long.df (data [1, , 1, wl.index = TRUE]),
+                                         dimension = 3)
+  use.x <- parsed.formula$right.x.name
+  use.y <- parsed.formula$right.y.name
+  use.z <- parsed.formula$left.name
+
+  dots <- list (...)
+  
+  ## if spc is used as z and the data set has multiple wavelengths cut and warn
+  if (use.z == "spc" && nwl (data) > 1 &&
+      !any (grepl (".wavelength", c(as.character (x),
+                                    as.character (dots$groups),
+                                    as.character (dots$subset))))) {
+    
+    data <- data [, , 1, wl.index = TRUE]
+    warning ("Only first wavelength is used for plotting")
+  }
+  
+  dots <- modifyList (list (xlab = data@label [[use.x]],
+                            ylab = data@label [[use.y]]),
+                      dots)
+
+  if (any (grepl ("spc", c(as.character (x),
+                           as.character (dots$groups),
+                           as.character (dots$subset)))))
+    data <- as.long.df (data)
+  else
+    data <- data$..
+
+  if (is.factor (data [[use.z]]) && transform.factor) {
+    dots <- trellis.factor.key (data [[use.z]], dots)
+    data [[use.z]] <- as.numeric (data [[use.z]])
+  }
+  
+  do.call(levelplot, c (list (x, data), dots))
+}
+
+setMethod ("levelplot", signature (x = "hyperSpec", data = "missing"),
+           function (x, data, ...) {
+             .levelplot (x = formula (spc ~ x * y), data = x, ...)
+           })
+setMethod ("levelplot", signature (x = "formula", data = "hyperSpec"), .levelplot)
+
+#################################################################################
+###
+###  plotmap - plot spectral maps
+###  
+###  plots intensity or extra data column over 2 extra data columns
+
+plotmap <- function (object, model = spc ~ x * y,
+                     func = mean, func.args = list (), ...){
+  .is.hy (object)
+  validObject (object)
+
+  if (nwl (object) > 1)
+    object <- do.call (apply, c (list (object, 1, func), func.args))
+  
+  dots <- modifyList (list (aspect = "iso"),
+                      list (...))
+                       
+  dots <- c (list (x = model, data = object), dots)
+
+  do.call(.levelplot, dots)
+}
+
+#################################################################################
+###
+###  plotmap - plot spectral maps
+###  
+###  plots intensity or extra data column over 2 extra data columns
+
+plotvoronoi <- function (object, model = spc ~ x * y,
+                         use.tripack = TRUE, mix = TRUE, ...){
+  if (use.tripack && mix)
+      object@data <- object@data [sample (nrow (object)),]
+
+  dots <- modifyList (list (object = object,
+                            model = model,
+                            panel = "panel.voronoi",
+                            prepanel = "prepanel.default.levelplot",
+                            pch = 19, cex = .25,
+                            use.tripack = use.tripack),
+                      list (...))
+  do.call (plotmap, dots)
+}
+
+map.identify <- function (object, model = spc ~ x * y, voronoi = FALSE, ...){
+  .is.hy (object)
+  validObject (object)
+
+  dots <- modifyList (list (object = object, model = model, ...),
+                      list (subscripts = TRUE))
+  if (voronoi) {
+    dots <- modifyList (list (col = "black", border = "#00000080"),
+                        dots)
+    dots <- modifyList (dots, list (mix = FALSE))
+    mix <- sample (nrow (object))
+    dots$object <- object [mix]
+    lattice <- do.call (plotvoronoi, dots)
+    mix <- order (mix)
+  }
+  else {
+    lattice <- do.call (plotmap, dots)
+    mix <- seq_len (nrow (object))
+  }
+
+  print (lattice)
+  trellis.focus ()
+
+  ## PROBLEM: panel.identify does _not_ keep the order of the selection.
+  ## End of input is right click, i.e. empty return value of panel.identify.
+  ## "no observations within 18 points" or the like should not break the loop, though.
+  ## Thus, count the warnings, and if the number increases, do not break.
+
+  .nwarn <- function () 
+    if (exists (last.warning)) length (last.warning) else 0
+  
+  nwarn <- .nwarn ()
+  res <- numeric (0)
+  repeat {
+    tmp <- panel.identify (x = lattice$panel.args.common$x [mix], 
+                           y = lattice$panel.args.common$y [mix],
+                           n = 1)
+
+    if (length (tmp) == 0 && .nwarn () == nwarn)
+      break
+    else
+      res <- c (res, tmp)
+
+    nwarn <- .nwarn ()
+  }
+
+  res
+}
+
