7ab972c48dbe45016ba938955741967eed587930
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2010-03-05 08:46:30 +0000

    housekeeping examples
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@211 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 6bd68067fdc3e1d7271ff8c9c852910783733d63

--- pkg/R/sample.R
+++ pkg/R/sample.R
@@ -1,44 +1,40 @@
 ###------------------------------------------------------------------------------
 ###
-###  make sample generic with ... argument
-###
-
-#setGeneric ("sample", signature = "x")
-
-setGeneric ("sample",
-            def = function (x, size, replace = FALSE, prob = NULL, ...){
-              standardGeneric ("sample")
-            },
-            useAsDefault = function (x, size, replace = FALSE, prob = NULL, ...){
-              base::sample (x = x, size = size, replace = replace, prob = prob, ...)
-            }
-)
-
-
-###------------------------------------------------------------------------------
-###
 ###  sample
 ###
 
 setMethod ("sample", signature = "hyperSpec",
-           function (x, size, replace = FALSE, prob = NULL,  
-                     index = FALSE,
-                     short = "sample", user = NULL, date = NULL) {
+           function (x, size, replace = FALSE, prob = NULL  
+                     ## short = "sample", user = NULL, date = NULL
+                     ## to be done in 2.11 - when sample is implicit Generic
+                     ) {
              validObject (x)
 
-             s <- sample.int (nrow (x), size = size, replace = replace, prob = prob)
+             s <- sample.int (nrow (x@data), size = size, replace = replace, prob = prob)
 
-             if (index)
-               s
-             else
-               .logentry (x [s], short = short,
-                          long = list (size = size, replace = replace,
-                            prob = prob, index = index),
-                          user = user, date = date)
+             .logentry (x [s], short = "sample",
+                        long = list (size = size, replace = replace, prob = prob))
+## for 2.11                        user = user, date = date)
            }
            )
 
-#setGenericImplicit ("sample")
+##' @rdname sample
+##' @export
+##' @author C. Beleites
+##' @seealso sample
+##' @param x
+##' @param size
+##' @param replace
+##' @param prob
+##' @return vector with indices suitable for row-indexing x
+##' @examples
+##' isample (chondro, 3)
+##' isample (chondro, 3, replace = FALSE)
+isample <- function (x, size, replace = FALSE, prob = NULL) {
+  .is.hy (x)
+  validObject (x)
 
+  sample.int (nrow (x), size = size, replace = replace, prob = prob)
+}
 
 
