91601ea68e7df4bef2ecb38bc92b60c11c35a4e2
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2010-06-21 12:05:24 +0000

    polygon selection
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@295 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: e841c6fe21fa18786404a385959f414a93f15e69

--- dev/cbfunctions.R
+++ dev/cbfunctions.R
@@ -384,3 +384,30 @@ t.array <- function (x){
   dims <- seq_along (dim (x))
   aperm (x, c (2, 1, dims [- (1 : 2)]))
 }
+
+
+sel.poly <- function (pch = 19, size = 0.3, ...){
+  trellis.focus ()
+
+  pts <- matrix (NA, nrow = 100, ncol = 2)
+  pos <- 1
+
+  repeat {
+    pt <- grid.locator (unit="native")
+    if (!is.null (pt)){
+      if (pos > nrow (pts))
+        pts <- cbind (pts, matrix (NA, nrow = 100, ncol = 2))
+      pts [pos, ] <- as.numeric (pt)
+      
+      grid.points (pt$x, pt$y, pch = pch, size = size, gp = gpar (...))
+      if (pos > 1)
+        grid.lines (unit (pts [(pos - 1) : pos, 1], "native"),
+                    unit (pts [(pos - 1) : pos, 2], "native"), gp = gpar (...))
+
+      pos <- pos + 1
+    } else 
+    break
+  }
+  
+  pts [seq_len (pos - 1),]
+}
