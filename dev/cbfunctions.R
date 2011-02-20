db8ec6d8371eb15c35cf68491341b62d17762dcc
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2011-02-20 21:45:06 +0000

    countrows -> arrayhelpers
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@375 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 6e2181889c30b7f59dd84623ca93e0f7a1deb1df

--- dev/cbfunctions.R
+++ dev/cbfunctions.R
@@ -1,30 +1,4 @@
 
-count.rows <- function(x) { 
-  if (is.matrix (x) && (dim (x) [2] == 1))
-    x <- as.vector (x) 
-
-  order.x <- do.call(order,as.data.frame(x))
-  if (is.vector (x)) {
-    equal.to.previous <-
-      x[tail(order.x,-1)] == x[head(order.x,-1)]
-  } else {
-    equal.to.previous <-
-      rowSums(x[tail(order.x,-1),] != x[head(order.x,-1),])==0
-  }
-
-  indices <- split (order.x, cumsum (c (TRUE, !equal.to.previous)))
-
-  if (is.vector (x)) {
-    x <- x [sapply (indices, function (v) v [[1]]),   drop = FALSE]
-  } else {
-    x <- x [sapply (indices, function (v) v [[1]]), , drop = FALSE]
-  }
-
-  data.frame (counts = sapply (indices, length),
-              ind    = I (indices),
-              x)
-}
-
 my.extract <- function (x, head, tail, ...){
   if (is.null (dim (x)))
     dim <- length (x)
