934ae39424d2d5e61b37b9ae4acec40ed84ea7ed
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2011-02-21 07:51:14 +0000

    move colSums & Co -> arrayhelpers
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@376 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: ca2f9a3963144567e8cfb535109c3a460d12cafe

--- dev/selpoly.R
+++ dev/selpoly.R
@@ -1,27 +1,24 @@
 sel.poly <- function (pch = 19, size = 0.3, ...){
   trellis.focus ()
 
-  pts <- matrix (NA, nrow = 100, ncol = 2)
-  pos <- 1
-
+  pts <- matrix (NA, nrow = 0, ncol = 2)
+  
   repeat {
     pt <- grid.locator (unit="native")
     if (!is.null (pt)){
-      if (pos > nrow (pts))
-        pts <- cbind (pts, matrix (NA, nrow = 100, ncol = 2))
-      pts [pos, ] <- as.numeric (pt)
+      pts <- rbind (as.numeric (pt))
       
       grid.points (unit (pts [pos, 1], "native"),
                    unit (pts [pos, 2], "native"), pch = pch,
                    size = unit (size, "char"), gp = gpar (...))
-      if (pos > 1)
-        grid.lines (unit (pts [(pos - 1) : pos, 1], "native"),
-                    unit (pts [(pos - 1) : pos, 2], "native"), gp = gpar (...))
-
-      pos <- pos + 1
-    } else 
-    break
+      
+      if (nrow (pts) > 1L)
+        grid.lines (unit (tail (pts [, 1L], 2L) , "native"),
+                    unit (tail (pts [, 2L], 2L) , "native"), gp = gpar (...))
+    } else {
+      break
+    }
   }
   
-  pts [seq_len (pos - 1),]
+  pts
 }
