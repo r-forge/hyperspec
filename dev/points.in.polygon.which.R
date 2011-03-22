09a317e273f144fad6aa3cd77d3ac4840ee29fa2
sebble (Sebastian Mellor) <rforge@sebble.com>
2011-03-22 09:41:10 +0000

    Added simple wrapper function for returing indices from hyperspec data and polygon
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@386 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: adda5f02023c50a0027fb08a24830993d1c759b3

---
+++ dev/points.in.polygon.which.R
@@ -0,0 +1,16 @@
+##' Wrapper for points.in.polygon ato return indices only
+
+points.in.polygon.which <- function (hyperspec, polygon.matrix){
+  
+  library(sp) # check for package first
+  pts <- point.in.polygon (hyperspec$x, hyperspec$y, 
+                           polygon.matrix [, 1], polygon.matrix [, 2]) # mode.checked=F
+  ind <- which ((pts>0), arr.ind=TRUE)
+  
+  ind
+}
+
+## Usage:
+## > plot (chondro, "map")
+## > poly <- sel.poly()
+## > points.in.polygon.which (chondro, poly)
