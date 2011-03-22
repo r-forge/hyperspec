d8132fc0aed303b3504bee4446d70dbe81908517
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2011-03-22 17:09:33 +0000

    questions for Seb
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@388 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 69856c7d9a2fffbb99399d69d9f512a89f22fb93

--- dev/points.in.polygon.which.R
+++ dev/points.in.polygon.which.R
@@ -3,9 +3,14 @@
 points.in.polygon.which <- function (hyperspec, polygon.matrix){
   
   require("sp") # should check for package first
+  ## CB: you have to check whether this was successful!, if you want to throw an error, use library
+  ## ()
+  
   pts <- point.in.polygon (hyperspec$x, hyperspec$y, 
                            polygon.matrix [, 1], polygon.matrix [, 2]) # mode.checked=F
   ind <- which ((pts>0), arr.ind=TRUE)
+  ## CB: why do you use arr.ind?
+  ## CB: why do you use which?
   
   ind
 }
--- dev/sel.map.poly.R
+++ dev/sel.map.poly.R
@@ -10,19 +10,25 @@
 
 ##' @return array of indices for points within selected polygon
 ##' @author Claudia Beleites, Sebastian Mellor
-##' @seealso \code{\link[grid]{grid.locator}}
-##' @seealso \code{\link[hyperspec]{sel.poly}}
+##' @seealso \code{\link[grid]{grid.locator}},  \code{\link{sel.poly}}, \code{\link{map.identify}}
+                                        # the package in [] in link is only needed for functions
+                                        # outside hyperSpec.
 ##' @export
 ##' @keywords iplot, hyperspec
 
 sel.map.poly <- function (data, pch = 19, size = 0.3, ...){
+  print (plotmap (data))
+  poly <- sel.poly(pch=pch, size=size, ...)
+  require("sp")# CB: you have to check whether this was successful! If you want to throw an error,
+               # use library ()
 
-  plot (data, "map")
-  #poly <- sel.poly(pch=pch, size=size, ...)
-  #require("sp")
-  #pts <- point.in.polygon (chondro$x, chondro$y, poly [, 1], poly [, 2]) #mode.checked=F
-  #ind <- which ((pts>0), arr.ind=TRUE)
+  pts <- point.in.polygon (chondro$x, chondro$y, poly [, 1], poly [, 2]) #mode.checked=F
+
+  ind <- which ((pts>0), arr.ind=TRUE)
+  ## CB: why do you use arr.ind?
+  ## CB: why do you use which?
+  ## CB: In other words: what is the advantage over just returning pts > 0 ?
   
-  #ind
+  ind
 }
 
