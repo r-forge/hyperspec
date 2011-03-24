3ee3d477cc8144aa9d8d3ba3599767a1550a4424
sebble (Sebastian Mellor) <rforge@sebble.com>
2011-03-24 08:31:53 +0000

    Added to documentation at top. replied to questions, wrapped require () in check, fixed bug (chondro$=>data$)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@393 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 6235bd665cfcd131aced3f99083e9a37e7c923cb

--- dev/sel.map.poly.R
+++ dev/sel.map.poly.R
@@ -1,33 +1,39 @@
 ##' Interactively select a polygon (grid graphics) and highlight points
 ##'
 ##' Click the points that should be connected as polygon. Input ends with right click (see
-##' \code{\link[grid]{grid.locator}}).
-
-####' @param pch symbol to display the points of the polygon
-####' @param size size for polygon point symbol
-####' @param ... further arguments for \code{\link[grid]{grid.points}} and
-####' \code{\link[grid]{grid.lines}}
-
+##' \code{\link[grid]{grid.locator}}). Polygon will be drawn closed.
+##' @param data hyperSpec object for plotting map
+##' @param pch symbol to display the points of the polygon for \code{\link{sel.poly}}
+##' @param size size for polygon point symbol for \code{\link{sel.poly}}
+##' @param ... further arguments for \code{\link[grid]{grid.points}} and
+##' \code{\link[grid]{grid.lines}}
 ##' @return array of indices for points within selected polygon
 ##' @author Claudia Beleites, Sebastian Mellor
 ##' @seealso \code{\link[grid]{grid.locator}},  \code{\link{sel.poly}}, \code{\link{map.identify}}
-                                        # the package in [] in link is only needed for functions
-                                        # outside hyperSpec.
 ##' @export
 ##' @keywords iplot, hyperspec
 
 sel.map.poly <- function (data, pch = 19, size = 0.3, ...){
   print (plotmap (data))
   poly <- sel.poly(pch=pch, size=size, ...)
-  require("sp")# CB: you have to check whether this was successful! If you want to throw an error,
-               # use library ()
+  
+  debuglevel <- hy.getOption ("debuglevel")
+  
+  if (!require("sp")){
+    cat("Error: SP package required for point.in.polygon ()\n")
+    stop() ## SM: What other error handling solutions are you currently using?
+           ##     Or should I just use library ()?
+  }
 
-  pts <- point.in.polygon (chondro$x, chondro$y, poly [, 1], poly [, 2]) #mode.checked=F
+  pts <- point.in.polygon (data$x, data$y, poly [, 1], poly [, 2]) #mode.checked=F
 
   ind <- which ((pts>0), arr.ind=TRUE)
   ## CB: why do you use arr.ind?
+  ## SM: some data types need it, i.e. matrices, I kept it in incase the structure ever changed
   ## CB: why do you use which?
   ## CB: In other words: what is the advantage over just returning pts > 0 ?
+  ## SM: with the polygon being drawn by hand I didn't think it important to differ points on the 
+  ##     edges or vertices from those within. Your thoughts?
   
   ind
 }
