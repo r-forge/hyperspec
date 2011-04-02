ad5925b387b89e0f11894dc09eecaca4f0b2204b
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2011-04-02 19:07:00 +0000

    pkg baseline in introduction
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@404 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 40519f974363ad5794b78a63a183c1c46fe0949e

--- pkg/R/extract-sqsq.R
+++ pkg/R/extract-sqsq.R
@@ -10,19 +10,20 @@ setMethod ("[[", "hyperSpec", function (x, i, j, l, ...,
   ## check wheter a index matrix is used
   if (! missing (i) && is.matrix (i)){
     if (! is.logical (i) && ! (is.numeric (i) && ncol (i) == 2))
-          stop ("Index matrix i  must either be logical of the size of x$spc,",
-                "or a n by 2 matrix.")
+      stop ("Index matrix i  must either be logical of the size of x$spc,",
+            "or a n by 2 matrix.")
 
     if (is.numeric (i) && ! wl.index) 
-        i [, 2] <- .getindex (x, i [, 2], extrapolate = FALSE)
+      i [, 2] <- .getindex (x, i [, 2], extrapolate = FALSE)
 
-    x@data$spc [i]                    # return value
+    x@data$spc [i]                      # return value
+    
   } else {                              # index by row and columns
     x <- .extract (x, i, j, l, ..., wl.index = wl.index)
     if (missing (j))
       unclass (x@data$spc[,, drop = drop]) # retrun value; removes the "AsIs"
     else {
-      x@data[,, drop = drop]            # return value
+      x@data[,, drop = drop]            # return value: data.frame
     }
   }
 })
