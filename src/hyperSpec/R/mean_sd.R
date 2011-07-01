5dca8c5452e0846dc40c9f1344542709bd2bba9c
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2011-07-01 15:19:10 +0000

    made mean_sd and mean_pm_sd generic, added faster methods for hyperSpec objects.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@439 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 02f02c08183ee119bc5e07d14143a0143dfe51ee

--- src/hyperSpec/R/mean_sd.R
+++ src/hyperSpec/R/mean_sd.R
@@ -4,39 +4,73 @@
 ##' 
 ##' These functions are provided for convenience.
 ##' 
-##' @aliases mean_sd
 ##' @rdname mean_sd
 ##' @param x a numeric vector
 ##' @param na.rm handed to \code{\link[base]{mean}} and \code{\link[stats]{sd}}
+##' @param \dots ignored (needed to make function generic)
 ##' @return \code{mean_sd} returns a vector with two values (mean and standard
 ##'   deviation) of \code{x}.
+##' @seealso \code{\link[base]{mean}}, \code{\link[stats]{sd}}
+##' @keywords multivar
+##' @export
+##' @examples
+##' 
+##' mean_sd (flu [,, 405 ~ 410])
+ 
+mean_sd <- function (x, na.rm = TRUE, ...)
+  c (mean = mean (x, na.rm = na.rm),
+     sd   = sd   (x, na.rm = na.rm)
+    )
+
+##' @rdname mean_sd
+##' @param short,user,date handed to \code{\link{logentry}}.
+##' @return \code{mean_sd} returns a hyperSpec object with the mean spectrum in the first row and the standard deviation in the 2nd.
 ##' @author C. Beleites
 ##' @seealso \code{\link[base]{mean}}, \code{\link[stats]{sd}}
 ##' @keywords univar
 ##' @export
 ##' @examples
 ##' 
-##'   mean_sd (flu$c)
- 
-mean_sd <- function (x, na.rm = TRUE)
-  c(mean = mean (x, na.rm = na.rm),
-    sd = sd (x, na.rm = na.rm)
-    )
+##' mean_sd (flu)
+setMethod ("mean_sd", signature = signature (x = "hyperSpec"),
+           function (x, na.rm = TRUE, ..., short = "mean_sd", user = NULL, date = NULL) {
+             m <- colMeans (x@data$spc)
+             s <- sd       (x@data$spc)
+             decomposition (x, rbind (mean = m, sd = s), scores = FALSE,
+                            short = short, user = user, date = date)
+           })
+
+
 
 ##' @aliases mean_pm_sd
 ##' @rdname mean_sd
 ##' @return
 ##'  
-##' \code{mean_pm_sd} returns a vector with 3 values: mean - 1 sd, mean, mean + 1 sd.
+##' \code{mean_pm_sd} returns a vector with 3 values: mean - 1 sd, mean, mean + 1 sd
 ##' @export
 ##' @examples
 ##' 
 ##'   mean_pm_sd (flu$c)
-##' 
-
-mean_pm_sd <- function (x, na.rm = TRUE){
+mean_pm_sd <- function (x, na.rm = TRUE, ...){
   m <- mean (x, na.rm = na.rm)
   s <- sd (x, na.rm = na.rm)
   c("mean.minus.sd" = m - s, "mean" = m, "mean.plus.sd" = m + s)
 }
 
+##' @rdname mean_sd
+##' @return For hyperSpec object, \code{mean_pm_sd} returns a hyperSpec object containing mean - sd,
+##' mean, and mean + sd spectra.
+##' @author C. Beleites
+##' @seealso \code{\link[base]{mean}}, \code{\link[stats]{sd}}
+##' @keywords univar
+##' @export
+##' @examples
+##' 
+##' mean_pm_sd (flu)
+setMethod ("mean_pm_sd", signature = signature (x = "hyperSpec"),
+           function (x, na.rm = TRUE, ..., short = "mean_sd", user = NULL, date = NULL) {
+             m <- colMeans (x@data$spc)
+             s <- sd       (x@data$spc)
+             decomposition (x, rbind ("mean - sd" = m - s, mean = m, "mean + sd"= m + s), 
+                            short = short, user = user, date = date)
+           })
