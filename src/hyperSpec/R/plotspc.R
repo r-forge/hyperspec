0490c98cd53430b55bb8dc182714f3d660c07e42
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2011-07-19 14:33:19 +0000

    bugfix: drop empty levels in stacked.offset
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@466 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: e0c14add806e053e447db90f2da1998b41180401

--- src/hyperSpec/R/plotspc.R
+++ src/hyperSpec/R/plotspc.R
@@ -482,27 +482,26 @@ plotspc <- function  (object,
 
 
 ##' y Offsets for Stacked Plots
-##' Calculate approriate \code{yoffset} values for stacking in
-##' \code{\link[hyperSpec]{plotspc}}.
+##' Calculate approriate \code{yoffset} values for stacking in \code{\link[hyperSpec]{plotspc}}.
 ##' 
-##' Usually, the \code{stacked} argument of \code{\link[hyperSpec]{plotspc}}
-##' will do fine, but if you need fine control over the stacking, you may
-##' calculate the y offsets yourself.
+##' Usually, the \code{stacked} argument of \code{\link[hyperSpec]{plotspc}} will do fine, but if you
+##' need fine control over the stacking, you may calculate the y offsets yourself.
+##'
+##' Empty levels of the stacking factor are dropped (as no stacking offset can be calculated in that
+##' case.)
 ##' 
 ##' @param x a \code{hyperSpec} object
-##' @param stacked \code{TRUE} to stack single spectra.  A numeric or factor is
-##'   interpreted as giving the grouping, a character is interpreted as the
-##'   name of the extra data column of \code{x} that holds the grouping.
-##' @param min.zero if \code{TRUE}, the lesser of zero and the minimum
-##'   intensity of the spectrum is used as minimum.
-##' @param add.factor,add.sum proportion and absolute amount of space that
-##'   should be added.
-##' @param .spc for internal use. If given, the ranges are evaluated on
-##'   \code{.spc}. However, this may change in future.
-##' @return a list containing \item{offsets}{numeric with the yoffset for each
-##'   group in \code{stacked}} \item{groups}{numeric with the group number for
-##'   each spectrum} \item{levels}{if \code{stacked} is a factor, the levels of
-##'   the groups}
+##' @param stacked \code{TRUE} to stack single spectra.  A numeric or factor is interpreted as giving
+##' the grouping, a character is interpreted as the name of the extra data column of \code{x} that
+##' holds the grouping.
+##' @param min.zero if \code{TRUE}, the lesser of zero and the minimum intensity of the spectrum is
+##' used as minimum.
+##' @param add.factor,add.sum proportion and absolute amount of space that should be added.
+##' @param .spc for internal use. If given, the ranges are evaluated on \code{.spc}. However, this
+##' may change in future.
+##' @return a list containing \item{offsets}{numeric with the yoffset for each group in
+##' \code{stacked}} \item{groups}{numeric with the group number for each spectrum} \item{levels}{if
+##' \code{stacked} is a factor, the levels of the groups}
 ##' @author C. Beleites
 ##' @seealso \code{\link[hyperSpec]{plotspc}}
 ##' @rdname plotspc
