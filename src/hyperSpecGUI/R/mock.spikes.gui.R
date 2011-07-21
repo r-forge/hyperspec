3d545955a77dc272af3de32479d9fea13be2b0bd
sebble (Sebastian Mellor) <rforge@sebble.com>
2011-07-21 12:34:36 +0000

    updated mock layout, look, and started mutaframe
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@471 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 33c38fa54a592eeb86596438d5d5b4ebb2f54906

--- src/hyperSpecGUI/R/mock.spikes.gui.R
+++ src/hyperSpecGUI/R/mock.spikes.gui.R
@@ -1,7 +1,14 @@
 library(gWidgets)
 library(hyperSpec)
+library(cranvas)
+library(plumbr) ## as.mutaframe
 options("guiToolkit"="Qt")
- 
+
+## Real notes:
+##   Haven't worked out how to extend a mutaframe (row-wise)
+##   ([[3]]<-c(1:5) adds col if ncol=2, nrow=5)
+##   So dim(return mutaframe) must be equal to highest possible spike count
+
 spikes.gui <- function() {
  
     ## gWidgets for Data
@@ -24,19 +31,19 @@ spikes.gui <- function() {
     BigGroup <- ggroup(cont=window)
     group <- ggroup(horizontal=FALSE, container=BigGroup)
     group2 <- ggroup(horizontal=FALSE, container=BigGroup)
-    group3 <- ggroup(container=group2)
+    group3 <- gpanedgroup(container=group2)
                                
-    tmp <- gframe("Sample size", container=group)
-    add(tmp,sampleSize)
-    add(tmp,reSample)
+    #tmp <- gframe("Sample size", container=group)
+    #add(tmp,sampleSize)
+    #add(tmp,reSample)
     
     #lapply(names(getStockIcons()), function(x) add(group, gbutton(x
     #forward, dismiss, ok
                                
-    add(gframe("Some slider", container=group),bandwidthAdjust,
-        expand=T)
+    #add(gframe("Spc Number", container=group),bandwidthAdjust,
+    #    expand=T)
 
-    add(gframe("Plotting options", container=group),reverseWL)
+    #add(gframe("Plotting options", container=group),reverseWL)
     #addStockIcons(
     #  c("next","drop","done"),
     #  c("/home/sebastian/tmp/hyperspec/pkg/hyperSpecGUI/inst/images/forward.gif",
@@ -45,7 +52,12 @@ spikes.gui <- function() {
     #iconNames <- c("done")
     #iconFiles <- c("/home/sebastian/tmp/hyperspec/pkg/hyperSpecGUI/inst/images/ok.gif")
     #addStockIcons(iconNames, iconFiles)
-    add(group, gtext("Spike suspicion: 1\n   Spectrum: 67\n[1] 1560.94\n[1] 2219.1\n  Spike points:  479 481\nSpikiness:  0.06663623 -1.423397"))
+#    add(group, glabel("<b>Spike suspicion</b>: 1<br />
+#                      <b>Spectrum</b>: 67<br />
+#                      (1560.94, 2219.1)<br />
+#                      <b>Spike points</b>:  479 481<br />
+#                      <b>Spikiness</b>:  0.06663623 -1.423397",
+#                      markup = TRUE))
 
     qparacetamol <- qdata(list('wavelength'=paracetamol@wavelength,'spc'=as.numeric(paracetamol@data$spc)))
     add(group3, qscatter(wavelength,spc,qparacetamol))
@@ -55,8 +67,26 @@ spikes.gui <- function() {
     tmp <- gframe("Spikes", container=group2)
     add(tmp, gbutton("Next Suspicion"))
     add(tmp,gimage('forward'))
-    add(tmp, gbutton("Drop Spectrum"))
+    add(tmp, gbutton("Good Spectrum"))
+    add(tmp,gimage('forward'))
+    add(tmp, gbutton("Bad Spectrum"))
     add(tmp,gimage('dismiss'))
     add(tmp, gbutton("Done"))
     add(tmp,gimage('ok'))
+    size(group3) <- c(75*9, 75*6)
+    
+    status <- gstatusbar("Suspicion 1 of 100", container=window)
+    
+    
+    add(group2, glabel("<b>Spike suspicion</b>: 1<br />
+                      <b>Spectrum</b>: 67<br />
+                      (1560.94, 2219.1)<br />
+                      <b>Spike points</b>:  479 481<br />
+                      <b>Spikiness</b>:  0.06663623 -1.423397",
+                      markup = TRUE))
+    
+    ## create mutaframe for spikes and return..
+    spikes <- as.mutaframe()
 }
+
+spikes.gui()
