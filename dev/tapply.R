f468479980172bd72ed1aade19cf1c40bb31260f
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2011-08-02 17:02:06 +0000

    tapply
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@499 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 4050b28d732064c456e2af75a9949200dffb38e9

--- dev/tapply.R
+++ dev/tapply.R
@@ -1,48 +1,56 @@
-.tapply <- function (X, INDEX, FUN = NULL, ..., levelorder = TRUE){
+.by <- function (data, INDICES, FUN = NULL, ..., levelorder = TRUE){
   
-  if (length (X$spc) == 0)
+  if (length (data$spc) == 0)
     stop ("empty spectra matrix.")
   
   FUN <- if (!is.null(FUN)) 
       match.fun(FUN)
-  if (!is.list(INDEX)) 
-      INDEX <- list(INDEX)
+
+  ## this function is internal. Checks should be done in the exported functions, so this one can be
+  ## as fast as possible.
+  ## INDICES should already be one factor
+  ##  if (!is.list(INDICES))  
+  ##      INDICES <- list(INDICES)
+browser ()
+  INDICES <- split (seq_len (nrow (data)), INDICES)
+
+  FUNx <- function (index) FUN (data$spc [index, ], ...)
   
+  spc <- lapply (INDICES, FUNx)
+
+  spc <- tapply (data@data$spc, INDICES, FUN, ...)
   
-  
-  spc <- tapply (X [, "spc", drop = FALSE], INDEX, FUN, ...)
-  
-  X
+  data
 }
 
 ##' .. content for \description{} (no empty lines) ..
 ##'
 ##' .. content for \details{} ..
 ##' @title tapply for hyperSpec objects
-##' @param X hyperSpec object
-##' @param INDEX factor giving the groups
+##' @param data hyperSpec object
+##' @param INDICES factor giving the groups
 ##' @param FUN aggregation function that works on a matrix
 ##' @param ... handed to \code{FUN}
 ##' @param simplify (doesn't apply to hyperSpec objects)
 ##' @param short,long,user,date handed to \code{\link{logentry}}
 ##' @param levelorder if TRUE, the rows of the result are in the order of the \code{levels
-##' (INDEX)}. Otherwise,
-##' @param drop should unused levels of \code{INDEX} be dropped?
+##' (INDICES)}. Otherwise,
+##' @param drop should unused levels of \code{INDICES} be dropped?
 ##' @return hyperSpec object
 ##' @author Claudia Beleites
-setMethod ("tapply", signature = signature (X = "hyperSpec", INDEX = "factor"),
-           function (X, INDEX, FUN = NULL, ...,
+setMethod ("by", signature = signature (data = "hyperSpec", INDICES = "factor"),
+           function (data, INDICES, FUN = NULL, ...,
                      simplify = stop ("not supported by hyperSpec objects"),
                      levelorder = TRUE, drop = TRUE, 
                      short = "tapply", long = NULL, user = NULL, date = NULL){
-             validObject (X)
+             validObject (data)
 
-             if (drop) INDEX <- droplevels (INDEX)
+             if (drop) INDICES <- droplevels (INDICES)
 
-             X <- .tapply (X@data, INDEX, FUN = FUN, ...)
+             data <- .tapply (data@data, INDICES, FUN = FUN, ...)
 
              ## FUN could have changed the no. of columns of the spectra matrix
-             validObject (X)
+             validObject (data)
 
-             .logentry(X, short = short, long = long, user = user, date = date)
+             .logentry(data, short = short, long = long, user = user, date = date)
            })
