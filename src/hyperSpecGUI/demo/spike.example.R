72f8cf6efaa49eb1b0513436fe26a6dc6777f1ad
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2011-08-11 11:47:00 +0000

    data in package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@523 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 6c0f6c6f4bf575fb1f4030e9a15735413b7e3a72

--- src/hyperSpecGUI/demo/spike.example.R
+++ src/hyperSpecGUI/demo/spike.example.R
@@ -9,10 +9,14 @@ tmp <- sweep (cartilage, 1, median, `/`)
 tmp <- sweep (tmp, 2, median, `-`)
 plot (tmp, "mat", col.regions = alois.palette ())
 scores <- spikefilter2d (spcmatrix = tmp [[]])
+spikes <- spikes.interactive (cartilage, scores)
+
+
+
 suspicions <- make.suspicions (scores)
 plot (suspicions[, "spikiness"])
 ## now use the original spectra and the suspicions from the preprocessed
 ## spectra for the interactive spike filter
-spikes <- spikes.interactive (cartilage, )
+spikes <- spikes.interactive.cb (cartilage, suspicions)
 spikes
 
