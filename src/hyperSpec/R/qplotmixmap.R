1e33f0bf72041aa25446f9e1208999e64e62909b
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2011-12-19 17:01:24 +0000

    typo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@636 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 0c4c68bfa8d78e587fe9fcd86582f750cc4516a2

--- src/hyperSpec/R/qplotmixmap.R
+++ src/hyperSpec/R/qplotmixmap.R
@@ -3,7 +3,7 @@
 ##' 
 ##' @title qplotmap with colour mixing for multivariate overlay
 ##' @param object hyperSpec object
-##' @param \dotshanded over to \code{\link[hyperSpec]{qmixtile}} and \code{\link[hyperSpec]{qmixlegend}}
+##' @param \dots handed over to \code{\link[hyperSpec]{qmixtile}} and \code{\link[hyperSpec]{qmixlegend}}
 ##' @return invisible list with ggplot2 objects map and legend
 ##' @seealso \code{\link[hyperSpec]{qmixtile}}
 ##' @author Claudia Beleites
@@ -62,7 +62,7 @@ legendright <- function (p, l, legend.width = 8, legend.unit = "lines") {
 ##' @param object matrix to be plotted with mixed colour channels
 ##' @param purecol pure component colours, names determine legend labels
 ##' @param mapping see \code{\link[ggplot2]{geom_tile}}
-##' @param \dots\code{qmixtile}: handed to \code{colmix.rgb}
+##' @param \dots \code{qmixtile}: handed to \code{colmix.rgb}
 qmixtile <- function (object,
                       purecol = stop ("pure component colors needed."),
                       mapping = aes_string (x = "x", y = "y", fill = "spc"),
@@ -156,7 +156,7 @@ normalize.minmax <- function (x, min = 0, max = 1, legend = FALSE, n = 100){
 ##' @param dx width of label bar
 ##' @param ny number of colours in legend
 ##' @param labels component names
-##' @param \dots\code{qmixlegend}: handed to \code{normalize}
+##' @param \dots \code{qmixlegend}: handed to \code{normalize}
 ##' @return ggplot object with legend
 ##' @author Claudia Beleites
 ##' @export 
