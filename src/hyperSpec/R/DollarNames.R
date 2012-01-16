12b3bcc7928aa99a2bd1eb6dc154139e3dfbd542
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2012-01-16 23:08:49 +0000

    rollback .Dollarnames S3 class
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@657 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 0b21ffef1dc8e8905540b27bac30a05c2a1f20a7

--- src/hyperSpec/R/DollarNames.R
+++ src/hyperSpec/R/DollarNames.R
@@ -3,14 +3,12 @@
 ##' @aliases .DollarNames .DollarNames,hyperSpec-method
 ##' @author C. Beleites
 ##' @seealso \code{\link[utils]{.DollarNames}}
-##' @export
+##' @export .DollarNames.hyperSpec
 ##' @rdname dollarnames
 ##' @keywords utilities
 ##' @title command line completion for $
 ##' @param x the hyperSpecobject
 ##' @param pattern pattern to look for
 ##' @return the name of the extra data slot
-##' @method .DollarNames hyperSpec
-##' @S3method .DollarNames hyperSpec
 .DollarNames.hyperSpec <- function (x, pattern)
   grep (pattern, colnames (x@data), value = TRUE)
