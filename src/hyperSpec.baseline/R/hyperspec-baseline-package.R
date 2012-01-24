f657450b61afc7ef315617f7f2fb2f9af834f53d
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2012-01-24 00:32:41 +0000

    baseline
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@661 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: b162feed403c5e5cfb3f0f9e18dbff9e82244aa0

---
+++ src/hyperSpec.baseline/R/hyperspec-baseline-package.R
@@ -0,0 +1,24 @@
+##' baseline for hyperSpec objects
+##' This package provides and interface to use the functions defined in \code{\link[baseline]{baseline-package}}
+##' with \code{\link[hyperSpec]{hyperSpec-class}} objects.
+##' 
+##' @name hyperSpec.baseline-package
+##' @title Package hyperSpec.baseline
+##' @docType package
+##' @author C. Beleites
+##' 
+##' Maintainer: Claudia Beleites <Claudia.Beleites@@ipht-jena.de>
+##' @seealso \code{citation ("hyperSpec-baseline")} produces the correct citation.
+##' 
+##' \code{package?hyperSpec.baseline} for information about the package
+##' @rdname hyperSpec.baseline-package
+##' @keywords package
+if (!require (svUnit, quietly = TRUE))
+  `.test<-` <- function (f, value) {
+    attr (f, "test") <- value
+    f
+ } else {
+ `.test<-` <- svUnit::`test<-`
+ }
+
+
