a2590547db4672964d557ff3d5aa6ebae5b3fa5d
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2012-03-23 17:54:36 +0000

    van der Monde matrix objects
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@677 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 5c75f825582f61aa0890c3188a6edf50aa021b00

---
+++ pkg/hyperSpec/R/vandermonde.R
@@ -0,0 +1,48 @@
+##' Function evaluation on hyperSpec objects
+##'
+##' vandermonde generates van der Monde matrices for the wavelengths of a hyperSpec object.
+##' @param x
+##' @param order of the polynomial
+##' @param \dots ignored by the standard generic
+##' @rdname vanderMonde
+##' @return van der Monde matrix
+##' @author C. Beleites
+vanderMonde <- function (x, order, ...){
+  outer (x, 0 : order, `^`)
+}
+
+##' @noRd
+setGeneric ("vanderMonde")
+
+##' @param \dots hyperSpec method: further arguments to \code{\link{decomposition}}
+##' @return hyperSpec method: hyperSpec object containing van der Monde matrix as spectra and an additional column ".vdm.order" giving the order of each spectrum (term).
+##' @rdname vanderMonde
+setMethod ("vanderMonde", signature = signature (x = "hyperSpec"), function (x, order, ...){
+  validObject (x)
+  
+  x <- decomposition (x, t (vanderMonde (x@wavelength, order)), scores = FALSE, ...)
+  x$.vdm.order <- 0 : order
+  x
+})
+
+
+
+##' @param x hyperSpec object
+##' @param \dots hyperSpec method: further arguments to \code{\link{decomposition}}
+##' @return hyperSpec object containing one spectrum for each expression 
+##' @rdname vanderMonde
+##' @examples
+##' plot (wl.eval (laser, exp = function (x) exp (-x)))
+wl.eval <- function (x, ...){
+  chk.hy (x)
+  validObject (x)
+
+  fun <- list (...)
+ 
+  x <- decomposition (x, t (sapply (fun, function (f) f (x@wavelength))), scores = FALSE, ...)
+  x$.f <- if (is.null (names (fun)))
+              rep (NA, length (fun))
+          else
+              names (fun)
+  x
+}
