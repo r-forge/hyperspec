77ce92fb8f1a2c78a8b1522ae4d509e15d9d08fc
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2012-03-28 10:09:20 +0000

    normalization option for vanderMonde and wl.eval
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@681 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 4cb2dbc3e20984154146288e57b01f0621fc2f53

--- pkg/hyperSpec/R/vandermonde.R
+++ pkg/hyperSpec/R/vandermonde.R
@@ -1,7 +1,7 @@
 ##' Function evaluation on hyperSpec objects
 ##'
 ##' vandermonde generates van der Monde matrices for the wavelengths of a hyperSpec object.
-##' @param x
+##' @param x vector with values to evaluate the polynomial on
 ##' @param order of the polynomial
 ##' @param \dots ignored by the standard generic
 ##' @rdname vanderMonde
@@ -12,21 +12,28 @@ vanderMonde <- function (x, order, ...){
   outer (x, 0 : order, `^`)
 }
 
-##' @nord
+##' @noRd
 setGeneric ("vanderMonde")
 
 ##' @param \dots hyperSpec method: further arguments to \code{\link{decomposition}}
+##' @param normalize.wl function to transorm the wavelengths before evaluating the polynomial (or
+##' other function). Use \code{normalize01} to map the wavelength range to the interval [0, 1].
 ##' @return hyperSpec method: hyperSpec object containing van der Monde matrix as spectra and an additional column ".vdm.order" giving the order of each spectrum (term).
 ##' @rdname vanderMonde
 ##' @export
-setMethod ("vanderMonde", signature = signature (x = "hyperSpec"), function (x, order, ...){
+##' @examples
+##' plot (vanderMonde (flu, 2))
+##' plot (vanderMonde (flu, 2, normalize.wl = normalize01))
+setMethod ("vanderMonde", signature = signature (x = "hyperSpec"),
+           function (x, order, ..., normalize.wl = I){
   validObject (x)
+
+  wl <- normalize.wl (x@wavelength)
   
-  x <- decomposition (x, t (vanderMonde (x@wavelength, order)), scores = FALSE, ...)
+  x <- decomposition (x, t (vanderMonde (wl, order)), scores = FALSE, ...)
   x$.vdm.order <- 0 : order
   x
 })
-
 
 
 ##' @param x hyperSpec object
@@ -36,16 +43,27 @@ setMethod ("vanderMonde", signature = signature (x = "hyperSpec"), function (x, 
 ##' @export
 ##' @examples
 ##' plot (wl.eval (laser, exp = function (x) exp (-x)))
-wl.eval <- function (x, ...){
+wl.eval <- function (x, ..., normalize.wl = I){
   chk.hy (x)
   validObject (x)
 
   fun <- list (...)
- 
-  x <- decomposition (x, t (sapply (fun, function (f) f (x@wavelength))), scores = FALSE, ...)
+
+  wl <- normalize.wl (x@wavelength)
+  
+  x <- decomposition (x, t (sapply (fun, function (f) f (wl))), scores = FALSE, ...)
   x$.f <- if (is.null (names (fun)))
               rep (NA, length (fun))
           else
               names (fun)
   x
+}
+
+##' @param x  vector with values to transform
+##' @return vector with \code{x} values mapped to the interval [0, 1]: (x - min (x)) / diff (range (x))
+##' @rdname vanderMonde
+##' @export 
+normalize01 <- function (x){
+  x <- x - min (x)
+  x / max (x)
 }
