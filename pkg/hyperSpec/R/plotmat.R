a96bc35bf5dc107df0a1c8f28f9de8050c4fb7cf
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2012-05-13 22:33:04 +0000

    build plotmat
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@711 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 1abd0750a0c4b6c7e610c42aeb32e3d58dc286c1

--- pkg/hyperSpec/R/plotmat.R
+++ pkg/hyperSpec/R/plotmat.R
@@ -1,7 +1,8 @@
 ##' Plot spectra matrix
 ##'
 ##' plots the spectra matrix
-##' @param x hyperSpec object
+##' @param object hyperSpec object
+##' @param y character giving the name of the extra data column to label the y axis.
 ##' @param ... further parameters for \code{\link[graphics]{image}}
 ##' @param contour should \code{\link[graphics]{contour}} be called instead of \code{\link[graphics]{image}}?
 ##' @author Claudia Beleites
@@ -9,21 +10,41 @@
 ##' @export 
 ##' @examples
 ##' plotmat (laser, col = alois.palette (100))
+##' 
 ##' plot (laser, "mat")
+##'
 ##' plotmat (laser)
 ##' plotmat (laser, contour = TRUE, add = TRUE)
-plotmat <- function (x, ..., contour = FALSE){
-  chk.hy (x)
-  validObject (x)
+##'
+##' ## use different y axis labels
+##'
+##' plotmat (laser, "t")
+##' 
+##' plotmat (laser, laser$t / 3600, ylab = "t / h")
+plotmat <- function (object, y = ".row", ylab, ..., contour = FALSE){
+
+  chk.hy (object)
+  validObject (object)
+
+  if (is.character (y)) {
+    if (missing (ylab))
+      ylab <- switch (y,
+                      .row = "row",
+                      labels (object, y))
+        
+    y <- switch (y,
+                 .row = seq_len (nrow (object)),
+                 object@data [[y]])
+  }
   
-  dots <- modifyList (list (x = wl (x),
-                            y = seq_len (nrow (x)),
-                            z = t (x [[]]),
-                            xlab = labels (x, ".wavelength"),
-                            ylab = "row"
+  dots <- modifyList (list (x = wl (object),
+                            y = y,
+                            z = t (object [[]]),
+                            xlab = labels (object, ".wavelength"),
+                            ylab = ylab
                             ),
                       list (...))
-
+  
   if (contour)
     do.call ("contour", dots)
   else
