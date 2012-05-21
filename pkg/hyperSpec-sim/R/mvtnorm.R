a805219befa6ec2aa3eaaf9374f74cfcd63d4f02
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2012-05-21 13:06:04 +0000

    intermediate
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@718 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 43108b2175e1afe2fe288c2414a0ba157dec0be5

--- pkg/hyperSpec-sim/R/mvtnorm.R
+++ pkg/hyperSpec-sim/R/mvtnorm.R
@@ -1,5 +1,4 @@
-
-.rmvnorm <- function (n, mean, sigma = cov (mean), ...) {
+.rmvnorm <- function (n, mean, sigma = cov (mean)) {
   .group <- rep.int (seq_along (n), n)
 
   data <- mean [.group]
@@ -9,54 +8,55 @@
 
   data$.group <- .group
 
-  .logentry (data, ...)
+  data
 }
 
+##' Multivariate normal random numbers
+##'
+##' Interface functions to use \code{\link[mvtnorm]{rmvnorm}} for
+##' \code{\link[hyperSpec]{hyperSpec-class}} objects.
+##'
+##' The \code{mvtnorm} method for hyperSpec objects supports producing multivariate normal data for
+##' groups with different mean but common covariance matrix, see the examples.
+##'
+##' @param n vector giving the numer of cases to generate for each group
+##' @param mean matrix with mean cases in rows
+##' @param sigma common covariance matrix
+##' @seealso \code{\link[mvtnorm]{rmvnorm}}
+##'
+##' \code{\link[hyperSpec]{cov}} and \code{\link[hyperSpec]{pooled.cov}} about calculating  covariance of hyperSpec objects.
+##' @rdname rmvnorm
+#
+#setGeneric ("rmvnorm",
+#            def = function (n,
+#              mean = rep(0, nrow(sigma)),
+#              sigma = diag(length(mean)), 
+#              method = c("eigen", "svd", "chol"), ...) {
+#              mvtnorm::rmvnorm (n, mean = mean, sigma = sigma, method = method)
+#            })
+#
+##' @rdname rmvnorm
+##' @examples
+##' ## multiple groups, common covariance matrix
+##' means <- aggregate (chondro, chondro$clusters, mean)
+##' pcov <- pooled.cov (chondro, chondro$clusters)
+##' rnd <- .rmvnorm (rep (1e2, 3), mean = pcov$mean, sigma = pcov$COV)
+##' 
+##' require ("MASS")
+##' tmp <- chondro [! is.na (chondro$clusters)]
+##' lda <- lda (x = tmp [[]], grouping = tmp$clusters)
+##' 
+##' p <- predict (lda, rnd [[]])
+##' 
+##' colors <- c("#00008040", "#FF000040", "#00800040")
+##' 
+##' plot (p$x, col = colors [p$class], pch = 3)
+##' 
+##' pc <- predict (lda)
+##' points (pc$x, col = colors [pc$class], pch = 20, cex = 0.5)
 
-rnd <- rmvnorm (rep (1e2, 3), x = chondro, groups = chondro$clusters)
-
-tmp <- chondro [! is.na (chondro$clusters)]
-lda <- lda (x = tmp [[]], grouping = tmp$clusters)
-
-p <- predict (lda, rnd [[]])
-plot (p$x, col = c("#00008040", "#FF000040", "#00800040") [p$class], pch = 3)
-
-pc <- predict (lda)
-points (pc$x, col = pc$class, pch = 20, cex = 0.5)
+setMethod ("rmvnorm", signature (n = "numeric", mean = "hyperSpec", sigma = "matrix"),
+           .rmvnorm)
 
 
-
-
-  setGeneric ("rmvnorm",
-            def = function (n,
-              mean = rep(0, nrow(sigma)),
-              sigma = diag(length(mean)), 
-              method = c("eigen", "svd", "chol"), ...) {
-              mvtnorm::rmvnorm (n, mean = mean, sigma = sigma, method = method)
-            })
-
-
-
-setMethod ("rmvnorm", signature (n = "numeric", mean = "hyperSpec", sigma = "matrix"), .rmvnorm)
-
-#setMethod ("rmvnorm", signature (n = "numeric", mean = "missing", sigma = "missing",
-#                                 x = "hyperSpec", groups = "factor"),
-rmvnorm <- function (n, x, groups, ..., reg = 1e-5 * max (abs (sigma))) {
-
-             if (any (is.na (groups))) {
-                 warning ("Removing NAs from groups.")
-                 x <- x [! is.na (groups)]
-                 groups <- groups [!is.na (groups)]
-               }
-             
-             mean <- aggregate (x, groups, "mean")
-             sigma <- cov (x [[]] - mean [[as.numeric (groups)]])
-
-             ## regularization
-             sigma <- sigma + diag (reg, nrow (sigma))
-             
-             .rmvnorm (n, mean = mean, sigma = sigma, ...)
-           }
-           
-#           )    
 
