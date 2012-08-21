cc0fd8b439d8c45032a725d6f21884a7f6762f10
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2012-08-21 18:34:18 +0000

    make mvtnorm suggestion, not dependency
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@782 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 41df9396f80a80fc5a5123299e7422aa3e544d97

---
+++ src/hyperSpec/R/mvtnorm.R
@@ -0,0 +1,110 @@
+##' @noRd
+if (require ("mvtnorm", quietly = TRUE)){
+  setGeneric ("rmvnorm", package = "mvtnorm")
+} else {
+  setGeneric ("rmvnorm", function (n, mean, sigma, ...) standardGeneric ("rmvnorm"))
+}
+
+.rmvnorm <- function (n, mean, sigma = cov (mean)) {
+  if (! require ("mvtnorm", quietly = TRUE))
+    stop ("Package mvtnorm required.")
+  
+  .group <- rep.int (seq_along (n), n)
+
+   ## make indices so that pooled or individual covariance matrices can be used.
+  if (length (dim (sigma)) == 3L)
+    isigma <- seq_len (dim (sigma) [3])
+  else {
+    isigma <- rep (1L, nrow (mean))
+    dim (sigma) <- c (dim (sigma), 1L)
+  }
+
+  tmp <- matrix (NA_real_, sum (n), ncol (mean))
+  for (i in seq_along (n))
+    tmp [.group == i,] <- mvtnorm::rmvnorm (n [i], mean [i,], sigma [,, isigma [i]])
+
+  attr (tmp, "group") <- .group
+
+  tmp
+}
+
+##' Multivariate normal random numbers
+##'
+##' Interface functions to use \code{\link[mvtnorm]{rmvnorm}} for
+##' \code{\link[hyperSpec]{hyperSpec-class}} objects.
+##'
+##' The \code{mvtnorm} method for hyperSpec objects supports producing multivariate normal data for
+##' groups with different mean but common covariance matrix, see the examples.
+##'
+##' @param n vector giving the numer of cases to generate for each group
+##' @param mean matrix with mean cases in rows
+##' @param sigma common covariance matrix or array (\code{ncol (mean)} x \code{ncol (mean)} x \code{nrow (mean)}) with individual covariance matrices for the groups.
+##' @export
+##' @seealso \code{\link[mvtnorm]{rmvnorm}}
+##'
+##' \code{\link[hyperSpec]{cov}} and \code{\link[hyperSpec]{pooled.cov}} about calculating  covariance of hyperSpec objects.
+##' @rdname rmvnorm
+##' @examples
+##' ## multiple groups, common covariance matrix
+##'
+##' 
+##' 
+##' pcov <- pooled.cov (chondro, chondro$clusters)
+##' rnd <- rmvnorm (rep (1e2, 3), mean = pcov$mean, sigma = pcov$COV)
+##' 
+##' require ("MASS")
+##' tmp <- chondro [! is.na (chondro$clusters)]
+##' lda <- lda (x = tmp [[]], grouping = tmp$clusters)
+##' 
+##' p <- predict (lda, rnd [[]])
+##' 
+##' colors <- c("#00008040", "#FF000040", "#00800040")
+##' 
+##' plot (p$x, col = colors [p$class], pch = 3)
+##' 
+##' pc <- predict (lda)
+##' points (pc$x, col = colors [pc$class], pch = 20, cex = 0.5)
+
+setMethod ("rmvnorm", signature (n = "numeric", mean = "hyperSpec", sigma = "matrix"),
+           function (n, mean, sigma){
+             tmp <- .rmvnorm (n, mean@data$spc, sigma)
+
+           data <- mean [attr (tmp, "group"),, drop = FALSE]
+           if (hy.getOption ("gc")) gc ()
+           data@data$spc <- tmp
+           if (hy.getOption ("gc")) gc ()
+           data$.group <- attr (tmp, "group")
+           if (hy.getOption ("gc")) gc ()
+           data
+           })
+
+##' @rdname rmvnorm
+##' @export
+setMethod ("rmvnorm", signature (n = "numeric", mean = "hyperSpec", sigma = "array"),
+           function (n, mean, sigma){
+             tmp <- .rmvnorm (n, mean@data$spc, sigma)
+
+           data <- mean [attr (tmp, "group"),, drop = FALSE]
+           if (hy.getOption ("gc")) gc ()
+           data@data$spc <- tmp
+           if (hy.getOption ("gc")) gc ()
+           data$.group <- attr (tmp, "group")
+           if (hy.getOption ("gc")) gc ()
+           data
+           })
+
+##' @rdname rmvnorm
+##' @export
+setMethod ("rmvnorm", signature (n = "numeric", mean = "matrix", sigma = "matrix"),
+           .rmvnorm)
+
+##' @rdname rmvnorm
+##' @export
+setMethod ("rmvnorm", signature (n = "numeric", mean = "matrix", sigma = "array"),
+           .rmvnorm)
+
+
+
+
+## produces matrices instead of hyperSpec objects. 
+## mapply (rmvnorm, n = 1:3, mean = pcov$mean, MoreArgs= list (sigma = pcov$COV), SIMPLIFY = FALSE))
