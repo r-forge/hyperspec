386f2980e2385b4f0e98c1c03d1d07ea0f0dc10b
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2012-11-01 14:04:35 +0000

    todo: fix lda$svd for rotated LDA!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@804 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 6580f50699b9e5ef2778f253d0510ab9233f41d4

---
+++ pkg/cbmodels/R/rotatelda.R
@@ -0,0 +1,56 @@
+##' \code{\link[MASS]{lda}} models are invariant to rotation, i.e. the predicted posterior
+##' probabilities and classes do not change if the LD scores \code{\link[MASS]{lda}$x} are rotated.
+##' @seealso 
+##' \code{\link[MASS]{lda}} 
+##' @export
+##' @include rotate.R
+##' @rdname rotate
+##' @examples
+##'
+##' ## clockwise rotation of lda
+##' model <- lda (Species ~ ., data = iris)
+##' pred <- predict (model)
+##' plot (pred$x, col = iris$Species, pch = 19, cex = 0.3, asp = 1)
+##' 
+##' model.rot <- rotate (model, rotmat (alpha = pi/40, ndim = ncol (model$scaling), 1, 2))
+##' pred.rot <- predict (model.rot)
+##' points (pred.rot$x, col = iris$Species, cex = 0.5)
+##'
+##' ## check difference between original model's predictions and rotated model's predictions
+##' diff <- pred$posterior - pred.rot$posterior
+##' summary (diff)
+##' boxplot (diff)
+rotate.lda <- function (object, R = NULL, ...){
+  if (nargs () > 2)
+      warning ("argument(s) ", paste (names (list (...)), collapse = ", "), " are ignored.")
+  
+  ndim <- ncol (object$scaling)
+  
+  ## check dimensions of R
+  if (length (dim (R)) != 2 || any (dim (R) != ndim))
+      stop ("R must be a square matrix of size (", ndim, " x ", ndim, ").")
+  ## check R R' = I
+  if (any (R %*% t (R) - diag (ndim) > .Machine$double.eps))
+      stop ("R %*% t (R) != diag (", ndim, ")")
+
+  object$scaling <- object$scaling %*% R
+
+  ## TODO: fix object$svd
+  ## guess: apply (predict (object, as.data.frame (object$means)), 2, sd) / object$counts;
+  ## Was ist mit prior?
+  
+  object
+}
+
+.test (rotate.lda) <- function () {
+  model <- lda (Species ~ ., data = iris)
+  pred <- predict (model)
+  ## plot (pred$x, col = iris$Species, pch = 19, cex = 0.5, asp = 1)
+
+  model.rot <- rotate (model, rotmat (alpha = 1, ndim = ncol (model$scaling), 1, 2))
+  pred.rot <- predict (model.rot)
+  ## points (pred.rot$x, col = iris$Species, cex = 0.5)
+  
+  checkEqualsNumeric (pred.rot$posterior, pred$posterior)
+}
+
