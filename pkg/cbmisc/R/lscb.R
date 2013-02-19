70e2ab700e857f8d13e3c3d77f475b0d120edf7d
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2013-02-19 14:43:00 +0000

    better version with total memory
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@847 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 15899f02cc96e63caae1347a4e26d189dbf6f949

--- pkg/cbmisc/R/lscb.R
+++ pkg/cbmisc/R/lscb.R
@@ -2,30 +2,40 @@
 ##'
 ##' @param names names of the object to list
 ##' @param sort column to sort
+##' @param total logical indicating whether the total size should be printed as well
 ##' @return data.frame with columns giving name, class, length, dim and size of the objects.
 ##' @author Claudia Beleites
 ##' @export
-lscb <- function (names = ls (envir = parent.frame ()), sort = "size"){
+lscb <- function (names = ls (envir = parent.frame ()), sort = "size", total = TRUE){
   size <- sapply (names, function (x) (object.size (get (x))))
   if (sort == "size")
     order <- order (size)
-
-  exp <- floor (log (size, 1024))
-  size <- size / 1024^exp
-  units <- c (" B", "kB", "MB", "GB", "TB") [exp + 1]
   
   res <- data.frame (name = names,
-              class = sapply (names, function (x) class (get (x))),
-              length = sapply (names, function (x) length (get (x))),
-              dim = sapply (names, function (x) paste (dim (get (x)), collapse = " x ")),
-              size = sprintf ("%0.0f %s", round (size), units)
-              )
+                     class = sapply (names, function (x) class (get (x))),
+                     length = sapply (names, function (x) length (get (x))),
+                     dim = sapply (names, function (x) paste (dim (get (x)), collapse = " x ")),
+                     size = .makeHumanSize (round (size))
+                     )
     
   rownames (res) <- NULL
 
   if (sort != "size")
     order <- order (res [[sort]])
   
-  res [order,]
+  res <- res [order,]
+  if (total)
+    res <- rbind (res, data.frame (name = "total",
+                                   class = "", length = nrow (res), dim = "",
+                                   size = if (nrow (res) == 0L) "0 B" else .makeHumanSize (sum (size))))
+
+  res
 }
 
+.makeHumanSize <- function (x){
+  exp <- floor (log (x, 1024))
+  x <- x / 1024^exp
+  units <- c (" B", "kB", "MB", "GB", "TB") [exp + 1]
+
+  sprintf ("%0.0f %s", round (x), units)
+}
