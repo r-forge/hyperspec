7b6099355ad38bd23a40e7891ea09e25f61ac12e
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2013-05-22 22:14:05 +0000

    spc.loess.R: NA warning only if NAs were actually generated. Drop logbook generation.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@859 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: c7cc564465c1d76467138748740d7c84aba73e32

--- src/hyperSpec/R/spc.loess.R
+++ src/hyperSpec/R/spc.loess.R
@@ -49,16 +49,13 @@ spc.loess <- function (spc, newx, enp.target = nwl (spc) / 4,
   chk.hy (spc)
   validObject (spc)
 
-  if (any (newx < min (spc@wavelength)) || any (newx > max (spc@wavelength)))
-    warning ("newx outside spectral range of spc. NAs will be generated.")
-
   loess <- apply (t (spc[[]]), 2, .loess, spc@wavelength)
 
   spc@data$spc <- t (sapply (loess, .predict, newx))
   .wl(spc) <- newx
 
-  .logentry (spc, short =  short,
-             long = list (newx = newx, enp.target = enp.target,
-               surface = surface, ...),
-             user = user, date = date)
+  if (any (is.na (spc@data$spc)))
+    warning ("NAs were generated. Most probably newx was outside the spectral range covered by spc.")
+
+  spc
 }
