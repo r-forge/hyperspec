e7abdcce72627f6ed0b1838301a0619b9ba14a9a
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2014-02-03 17:07:43 +0000

    bugfix: only named arguments to all.equal
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@893 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 1920c80754ef23c62e0c5d6919b1bbb76569bd71

--- pkg/hyperSpec/R/all.equal.R
+++ pkg/hyperSpec/R/all.equal.R
@@ -28,24 +28,29 @@ setMethod ("all.equal", signature (target = "hyperSpec", current = "hyperSpec"),
 
              result <- character (0)
 
-             cmp <- all.equal (target@wavelength, current@wavelength, check.attributes, check.names, ...)
+             cmp <- all.equal (target = target@wavelength, current = current@wavelength,
+                               check.attributes = check.attributes, check.names = check.names, ...)
              if (! isTRUE (cmp)) result <- c("@wavelength:", cmp)
 
              if (check.column.order)
-               cmp <- all.equal (target@data, current@data, check.attributes, ...)
+               cmp <- all.equal (target = target@data, current = current@data,
+                                 check.attributes = check.attributes, ...)
              else
-               cmp <- all.equal ( target@data[order (colnames ( target@data))],
-                                 current@data[order (colnames (current@data))], check.attributes, check.names, ...)
+               cmp <- all.equal (target  = target@data  [order (colnames ( target@data))],
+                                 current = current@data [order (colnames (current@data))],
+                                 check.attributes = check.attributes, check.names = check.names, ...)
              if (! isTRUE (cmp)) result <- c (result, "@data:", cmp)
 
              if (check.label){
-               cmp <- all.equal (target@label[order (names (target@label))],
-                                 current@label[order (names (current@label))], check.attributes, check.names, ...)
+               cmp <- all.equal (target  = target@label  [order (names (target@label))],
+                                 current = current@label [order (names (current@label))],
+                                 check.attributes = check.attributes, check.names = check.names, ...)
                if (! isTRUE (cmp)) result <- c (result, "@label:", cmp)
              }
 
              if (check.log) {
-               cmp <- all.equal (target@log, current@log, check.attributes, check.names, ...)
+               cmp <- all.equal (target = target@log, current = current@log,
+                                 check.attributes = check.attributes, check.names = check.names, ...)
                if (! isTRUE (cmp)) result <- c (result, "@log:", cmp)
              }
 
