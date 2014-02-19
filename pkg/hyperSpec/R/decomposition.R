9aa79b2a3c43b18d4008a3d592fe7ff38b4d275f
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2014-02-19 22:32:50 +0000

    build
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@945 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: fce4aa59cd922643051022a3d6f8870f8440f94e

--- pkg/hyperSpec/R/decomposition.R
+++ pkg/hyperSpec/R/decomposition.R
@@ -19,16 +19,13 @@
 ##' the same value for all spectra are set to \code{NA}.  Thus, for the resulting \code{hyperSpec}
 ##' object, \code{\link{plotspc}} and related functions are meaningful.
 ##' \code{\link[hyperSpec]{plotmap}} cannot be applied as the loadings are not laterally resolved.
-##' 
+##'  
 ##' The scores matrix needs to have the same number of rows as \code{object} has spectra. If such a
-##' matrix is given, \code{latentscores} replaces the spectra matrix \code{x} and
+##' matrix is given, \code{decomposition} will replace the spectra matrix is replaced by \code{x} and
 ##' \code{object@@wavelength} by \code{wavelength}. The information related to each of the spectra is
 ##' retained. For such a \code{hyperSpec} object, \code{\link{plotmap}} and \code{\link{plotc}} and
-##' the like can be applied. Of couse, it is also possible to use the spectra plotting, but the
+##' the like can be applied. It is also possible to use the spectra plotting, but the
 ##' interpretation is not that of the spectrum any longer.
-##'
-##' In order to avoid incompatibility with package \code{pls}, \code{latentvars} and
-##' \code{latentscores}.
 ##' 
 ##' @param object A \code{hyperSpec} object.
 ##' @param x matrix with the new content for \code{object@@data$spc}.
@@ -70,7 +67,7 @@ decomposition <- function (object, x, wavelength = seq_len (ncol (x)),
                            scores = TRUE, retain.columns = FALSE,
                            short = "decomposition", user = NULL, date = NULL,
                            ...){
-#  message ("decomposition will be deprecated: please change your code to use `latentvars` or `latentscores` instead.")
+#  message ("decomposition will be deprecated: please change your code to use `loadings` or `scores` instead.")
   
   validObject (object)
 
