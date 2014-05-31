0f9fdf59ba50ed35179da57f57e37c5567ceeb3c
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2014-05-31 11:41:22 +0000

    include data first
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@961 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 08ff4ff214d5ab0e7676ef6f74bcd89ce1c060bd

--- pkg/hyperSpec/R/hyperspec-package.R
+++ pkg/hyperSpec/R/hyperspec-package.R
@@ -24,6 +24,11 @@
 ##' \code{class?hyperSpec} for details on the S4 class provided by this
 ##'   package.
 ##' @rdname hyperSpec-package
+##' @include flu.R
+##' @include chondro.R
+##' @include laser.R
+##' @include paracetamol.R
+##' @include barbiturates.R
 ##' @keywords package
 if (!require ("svUnit", quietly = TRUE)){
   `.test<-` <- function (f, value) {
