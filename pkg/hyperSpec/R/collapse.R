f690598e10110d3fe1735e259a5eb96ef356ff3b
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2015-02-12 23:11:54 +0000

    bugfix: collapse now returns object names in column .name and returns correct labels
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@1051 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 1ca0e454a1daf6e9030d16548477a7f1fdceaf64

--- pkg/hyperSpec/R/collapse.R
+++ pkg/hyperSpec/R/collapse.R
@@ -5,13 +5,16 @@
 ##' Data points corresponding to wavelengths not in the original spectrum will be set to NA.
 ##' Extra data is combined in the same manner.
 ##' 
+##' If the objects are named, the names will be preserved in extra data column \code{$.name}.
+##' 
 ##' @author C. Beleites
 ##' @title Collapse hyperSpec objects
 ##' @export
 ##' @param ... hyperSpec objects to be collapsed into one object. Instead of giving several
 ##' arguments, a list with all objects to be collapsed may be given.
 ##' @aliases collapse collapse.hyperSpec
-##' @seealso \code{\link[base]{merge}} to merge hyperSpec objects that share wavelengths but contain different spectra,  \code{\link[base]{rbind}}, and  \code{\link[plyr]{rbind.fill}} for 
+##' @seealso \code{\link[base]{merge}} to merge hyperSpec objects that share wavelengths but contain 
+##'   different spectra,  \code{\link[base]{rbind}}, and  \code{\link[plyr]{rbind.fill}} for 
 ##' @return a hyperSpec object
 ##' @keywords manip
 ##' @examples
@@ -40,6 +43,13 @@ collapse <- function (...){
   lapply (dots, chk.hy)
   lapply (dots, validObject)
 
+  ## names cause problems with unlisting labels.
+  ## preserve them in column .name
+  if (! is.null (names (dots))){
+    dots <- mapply (function (object, name) {object$.name <- name; object}, dots, names (dots))
+    names (dots) <- NULL
+  }  
+  
   ## prepare new labels
   labels <- unlist (lapply (dots, slot, "label"))
   labels <- labels [unique (names (labels))]
@@ -60,3 +70,20 @@ collapse <- function (...){
   x
 }
 
+.test (collapse) <- function () {
+  ## collapse messed up labels if a named list is collapsed
+  tmp <- collapse (a = flu, b = flu)
+  flu.labels <- lapply (flu@label, as.expression)
+  checkEquals (labels (tmp) [names (flu.labels)], flu.labels)
+
+  ## named lists should return .name column
+  checkEquals (tmp$.name, rep (c ("a", "b"), each = nrow (flu)))
+  
+  ## no difference whether list or single arguments are given
+  tmp2 <- list (a = flu, b = flu)
+  tmp2 <- collapse (a = flu, b = flu)
+  checkEquals (tmp, tmp2, 
+               check.attributes = TRUE, check.names = TRUE, check.column.order = FALSE, check.label = TRUE)
+}
+
+## FIXME: 
