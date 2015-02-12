b0a723990a42c0c2614479fd8f0ded0a00245000
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2015-02-12 23:11:58 +0000

    refactor: move definition into internal function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@1055 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 46447051493923eeff34411566565674c17219bf

--- pkg/hyperSpec/R/all.equal.R
+++ pkg/hyperSpec/R/all.equal.R
@@ -1,3 +1,40 @@
+.all.equal <- function (target, current, ..., check.attributes = FALSE, check.names = FALSE, 
+                        check.column.order = FALSE, check.label = FALSE){
+  validObject (target)
+  validObject (current)
+  
+  result <- character (0)
+  
+  cmp <- all.equal (target = target@wavelength, current = current@wavelength, ...,
+                    check.attributes = check.attributes, check.names = check.names)
+  if (! isTRUE (cmp)) result <- c("@wavelength:", cmp)
+  
+  if (check.column.order)
+    cmp <- all.equal (target = target@data, current = current@data, ...,
+                      check.attributes = check.attributes)
+  else
+    cmp <- all.equal (target  = target@data  [order (colnames ( target@data))],
+                      current = current@data [order (colnames (current@data))],
+                      ...,
+                      check.attributes = check.attributes, check.names = check.names)
+  if (! isTRUE (cmp)) result <- c (result, "@data:", cmp)
+  
+  if (check.label){
+    cmp <- all.equal (target  = target@label  [order (names (target@label))],
+                      current = current@label [order (names (current@label))],
+                      ...,
+                      check.attributes = check.attributes, check.names = check.names)
+    if (! isTRUE (cmp)) result <- c (result, "@label:", cmp)
+  }
+  
+  if (length (result) == 0)
+    TRUE
+  else
+    result
+}
+
+
+
 ##' @aliases all.equal  all.equal,hyperSpec,hyperSpec-method
 ##' @rdname Comparison
 ##' @param target,current two \code{hyperSpec} objects that are tested for
@@ -19,41 +56,4 @@
 ##' @export
 ##' @examples
 ##' 
-##' all.equal (flu, --flu);
-setMethod ("all.equal", signature (target = "hyperSpec", current = "hyperSpec"),
-           function (target, current, ..., check.attributes = FALSE, check.names = FALSE, 
-                     check.column.order = FALSE, check.label = FALSE){
-             validObject (target)
-             validObject (current)
-
-             result <- character (0)
-
-             cmp <- all.equal (target = target@wavelength, current = current@wavelength, ...,
-                               check.attributes = check.attributes, check.names = check.names)
-             if (! isTRUE (cmp)) result <- c("@wavelength:", cmp)
-
-             if (check.column.order)
-               cmp <- all.equal (target = target@data, current = current@data, ...,
-                                 check.attributes = check.attributes)
-             else
-               cmp <- all.equal (target  = target@data  [order (colnames ( target@data))],
-                                 current = current@data [order (colnames (current@data))],
-                                 ...,
-                                 check.attributes = check.attributes, check.names = check.names)
-             if (! isTRUE (cmp)) result <- c (result, "@data:", cmp)
-
-             if (check.label){
-               cmp <- all.equal (target  = target@label  [order (names (target@label))],
-                                 current = current@label [order (names (current@label))],
-                                 ...,
-                                 check.attributes = check.attributes, check.names = check.names)
-               if (! isTRUE (cmp)) result <- c (result, "@label:", cmp)
-             }
-
-             if (length (result) == 0)
-               TRUE
-             else
-               result
-           }
-           )
-
+setMethod ("all.equal", signature (target = "hyperSpec", current = "hyperSpec"), .all.equal)
