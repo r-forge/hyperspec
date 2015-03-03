3c593d217616166ff73a90b5c1634f3420b000db
cbeleites (Claudia Beleites) <chemometrie@beleites.de>
2015-03-03 18:54:53 +0000

    bugfix: tolerance was missing. bugfix: if all spectra are removed, do not attempt to assign file name
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@1083 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 74894bfc3eb15bdcb4db68eafbd21010fd4766eb

--- pkg/hyperSpec/R/fileio.optional.R
+++ pkg/hyperSpec/R/fileio.optional.R
@@ -6,6 +6,12 @@
                               file.remove.emptyspc = hy.getOption ("file.remove.emptyspc"),
                               file.keep.name = hy.getOption ("file.keep.name")){
   
+	tolerance <- hy.getOption ("tolerance")
+	if (is.null (tolerance)){
+		warning ("hyperSpec option tolerance unset -> setting to .Machine$double.eps (", .Machine$double.eps, ')')
+		hy.setOptions (tolerance = .Machine$double.eps)
+	}
+	
   if (file.remove.emptyspc) {
     ## remove spectra consisting of 0 only
     spc <- spc [rowSums (abs (spc) < tolerance) < nwl (spc)]
@@ -14,7 +20,7 @@
     spc <- spc [rowSums (is.na (spc)) < nwl (spc)]
   }
   
-  if (file.keep.name){
+  if (file.keep.name & nrow (spc) > 0L){
     if (is.null (spc@data$filename)){ 
       spc@data$filename <- filename
       spc@label$filename <- "filename"
