380f70898844d3105958a4bea7e1ef9a215128e1
mdahms (Marcel Dahms) <marceldahms@gmx.de>
2015-07-17 12:54:22 +0000

    scan.txt.Witec.R: changed order function together with parameter descriptions according to file name
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hyperspec@1153 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 0cca13fadcdb1b2d92503180fd2d9a032593b049

--- pkg/hyperSpec/R/scan.txt.Witec.R
+++ pkg/hyperSpec/R/scan.txt.Witec.R
@@ -1,12 +1,10 @@
-
 ##' Import Raman Spectra/Maps from Witec Instrument via ASCII files
 ##'
-##' \code{scan.dat.Witec} reads Witec's ASCII exported data which comes in separate files with x and
-##' y data. \code{scan.txt.Witec} reads Witec ASCII files where the first column gives the wavelength
-##' axes and the other columns the spectra.
+##' \code{scan.txt.Witec} reads Witec ASCII files where the first column gives the wavelength
+##' axes and the other columns the spectra. \code{scan.dat.Witec} reads Witec's ASCII exported data 
+##' which comes in separate files with x and y data.
 ##' @title File Import Witec Raman
-##' @param filex filename wavelength axis file
-##' @param filey filename intensity file
+##' @param file filename or connection to ASCII file
 ##' @param points.per.line number of spectra in x direction of the map
 ##' @param lines.per.image number of spectra in y direction
 ##' @param ...,quiet handed to \code{\link[base]{scan}}
@@ -19,68 +17,70 @@
 ##'
 ##' \code{\link{options}} for details on options.
 ##' @export
-scan.dat.Witec <- function (filex = stop ("filename or connection needed"),
-                            filey = sub ("-x", "-y", filex),
-                            points.per.line = NULL,
-                            lines.per.image = NULL,
-                            ...,
-                            quiet = hy.getOption ("debuglevel") < 1L){
-  wl <- scan (file = filex, ..., quiet = quiet)
-  spc <- scan (file = filey, ..., quiet = quiet)
-
-  dim (spc) <- c (length (wl), length (spc) / length (wl))
-
-  spc <- new ("hyperSpec", wavelength = wl, spc = t (spc))
-
-  if (!is.null (points.per.line))
-    spc@data$x <- rep (seq_len (points.per.line), lines.per.image)
-
-  if (!is.null (lines.per.image))
-    spc@data$y <- rep (- seq_len (lines.per.image), each = points.per.line)
-
-  ## consistent file import behaviour across import functions
-  .fileio.optional (spc, filey)
-}
-
-##' @rdname scan.dat.Witec
-##' @param file filename or connection to ASCII file
-##' @export
 scan.txt.Witec <- function (file = stop ("filename or connection needed"),
                             points.per.line = NULL,
                             lines.per.image = NULL,
                             nwl = 1024,
                             remove.zerospc = TRUE,
                             ...){
-
+    
     if (!missing (remove.zerospc))
         .Deprecated (new = "hy.setOptions (file.remove.emptyspc = TRUE)", 
                      package = "hyperSpec",
                      old = "remove.zerospc"
-                     )
-        
-  if (is.null (nwl)){
-    txt <- readLines (file)
-    nwl <- length (txt)
-    txt <- scan (text = txt, ...)
-  } else {
-    txt <- scan (file, ...)
-  }
+        )
+    
+    if (is.null (nwl)){
+        txt <- readLines (file)
+        nwl <- length (txt)
+        txt <- scan (text = txt, ...)
+    } else {
+        txt <- scan (file, ...)
+    }
+    
+    dim (txt) <- c (length (txt) / nwl, nwl)
+    
+    ## fix: Witec/Andor may have final comma without values -> last line is NA only
+    ## => delete last row if this happens for a map
+    if (all (is.na (txt [nrow (txt), ])))
+        txt <- txt [- nrow (txt), ]
+    
+    spc <- new ("hyperSpec", wavelength = txt [1, ], spc = txt [-1, ])
+    
+    if (!is.null (points.per.line))
+        spc@data$x <- rep (seq_len (points.per.line), lines.per.image)
+    
+    if (!is.null (lines.per.image))
+        spc@data$y <- rep (- seq_len (lines.per.image), each = points.per.line)
+    
+    ## consistent file import behaviour across import functions
+    .fileio.optional (spc, file)
+}
 
-  dim (txt) <- c (length (txt) / nwl, nwl)
 
-  ## fix: Witec/Andor may have final comma without values -> last line is NA only
-  ## => delete last row if this happens for a map
-  if (all (is.na (txt [nrow (txt), ])))
-    txt <- txt [- nrow (txt), ]
-
-  spc <- new ("hyperSpec", wavelength = txt [1, ], spc = txt [-1, ])
-
-  if (!is.null (points.per.line))
-    spc@data$x <- rep (seq_len (points.per.line), lines.per.image)
-
-  if (!is.null (lines.per.image))
-    spc@data$y <- rep (- seq_len (lines.per.image), each = points.per.line)
-
-  ## consistent file import behaviour across import functions
-  .fileio.optional (spc, file)
+##' @rdname scan.txt.Witec
+##' @param filex filename wavelength axis file
+##' @param filey filename intensity file
+##' @export
+scan.dat.Witec <- function (filex = stop ("filename or connection needed"),
+filey = sub ("-x", "-y", filex),
+points.per.line = NULL,
+lines.per.image = NULL,
+...,
+quiet = hy.getOption ("debuglevel") < 1L){
+    wl <- scan (file = filex, ..., quiet = quiet)
+    spc <- scan (file = filey, ..., quiet = quiet)
+    
+    dim (spc) <- c (length (wl), length (spc) / length (wl))
+    
+    spc <- new ("hyperSpec", wavelength = wl, spc = t (spc))
+    
+    if (!is.null (points.per.line))
+        spc@data$x <- rep (seq_len (points.per.line), lines.per.image)
+    
+    if (!is.null (lines.per.image))
+        spc@data$y <- rep (- seq_len (lines.per.image), each = points.per.line)
+    
+    ## consistent file import behaviour across import functions
+    .fileio.optional (spc, filey)
 }
